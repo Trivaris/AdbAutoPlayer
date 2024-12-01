e52f022fcc51dd7b7c034e390a9f97782975c7a6
yulesxoxo <189644144+yulesxoxo@users.noreply.github.com>
2024-12-01 13:44:36 +0100

    wip

--- adb_auto_player/frontend/src/routes/+page.svelte
+++ adb_auto_player/frontend/src/routes/+page.svelte
@@ -1,22 +1,58 @@
 <script lang="ts">
     import CommandPanel from "./CommandPanel.svelte";
 
-    let disabled = false;
+    let disableActions = false;
+    let game: string | null = null;
+    let games: string[] | null = null;
 
-    function eelTest(event: Event) {
+    function updateMenu() {
+        window.eel?.get_menu()((response: string[] | null) => {
+            // TODO
+            // if the response differs from games
+            // clear all the buttons in the commandpanel component
+            games = response;
+            if (null === games) {
+                return;
+            }
+            // Add buttons to the commandpanel component
+            // one button for each string
+            // the button text should be the string
+            // it should execute the executeMenuItem on click
+            // and pass the index of the string from this array
+        });
+    }
+
+    function updateGame() {
+        if (disableActions) {
+            return;
+        }
+        window.eel?.get_running_supported_game()((response: null|string) => {
+            if (game != response) {
+                updateMenu();
+                game = response;
+            }
+        });
+    }
+
+    updateGame();
+    setInterval(updateGame, 3000);
+
+    function executeMenuItem(event: Event) {
         event.preventDefault();
-        disabled = true;
-        window.eel?.test_bla_bla()((response: string) => {
-            console.log(response);
-            disabled = false;
+        disableActions = true;
+        window.eel?.execute(
+            // TODO Button index
+        )((response: null|string) => {
+            disableActions = false;
         });
     }
 </script>
 
 <main class="container">
-    <h1>Adb Auto Player</h1>
+    <h1>{game ? game : "Please start a supported game"}</h1>
+
     <CommandPanel title={"Menu"}>
-        <button {disabled} onclick={eelTest} >Push Season Talent Stages</button>
+        Placeholder
     </CommandPanel>
 </main>
 
