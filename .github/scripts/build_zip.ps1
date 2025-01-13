239dda2633438862680b79760c2b00e361071ae5
yulesxoxo <189644144+yulesxoxo@users.noreply.github.com>
2025-01-13 17:26:58 +0100

    fix build_zip.ps1

--- .github/scripts/build_zip.ps1
+++ .github/scripts/build_zip.ps1
@@ -2,13 +2,13 @@ $Workspace = $env:GITHUB_WORKSPACE
 $ReleaseZipDir = Join-Path $Workspace "release_zip"
 
 New-Item -ItemType Directory -Force -Path $ReleaseZipDir
+$TemplatesDir = Join-Path $ReleaseZipDir "games/afk_journey/templates"
+New-Item -ItemType Directory -Force -Path $TemplatesDir
 
 Copy-Item -Path "cmd/wails/build/bin/AdbAutoPlayer.exe" -Destination $ReleaseZipDir -Force
 Copy-Item -Path "cmd/wails/config.toml" -Destination $ReleaseZipDir -Force
-Copy-Item -Path "python/adb_auto_player.exe" -Destination $ReleaseZipDir -Force
+Copy-Item -Path "python/adb_auto_player.exe" -Destination "$ReleaseZipDir/games" -Force
 
-$TemplatesDir = Join-Path $ReleaseZipDir "games/afk_journey/templates"
-New-Item -ItemType Directory -Force -Path $TemplatesDir
 Copy-Item -Path "python/adb_auto_player/games/afk_journey/templates/*" -Destination $TemplatesDir -Recurse -Force
 Copy-Item -Path "python/adb_auto_player/games/afk_journey/AFKJourney.toml" -Destination (Join-Path $ReleaseZipDir "games/afk_journey") -Force
 
