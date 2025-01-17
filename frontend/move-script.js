4e3940f8c9566688f8253e78732ebb2da77c6857
yulesxoxo <189644144+yulesxoxo@users.noreply.github.com>
2025-01-17 21:06:30 +0100

    fix build

---
+++ frontend/move-script.js
@@ -0,0 +1,18 @@
+import fs from "fs-extra";
+import path from "path";
+
+async function moveBuildDir() {
+  const buildDir = path.resolve("build");
+  const targetDir = path.resolve("..", "cmd", "wails", "frontend", "build");
+
+  try {
+    await fs.ensureDir(targetDir);
+
+    await fs.copy(buildDir, targetDir, { overwrite: false });
+    console.log("Build directory moved successfully!");
+  } catch (error) {
+    console.error("Error moving build directory:", error);
+  }
+}
+
+moveBuildDir();
