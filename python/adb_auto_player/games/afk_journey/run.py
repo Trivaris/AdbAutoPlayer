c032f71ac8b3f4372e78c74cb11a3cd9bd07e08f
yulesxoxo <189644144+yulesxoxo@users.noreply.github.com>
2025-01-20 05:58:05 +0100

    Dura's Trial Nightmare mode

--- python/adb_auto_player/games/afk_journey/run.py
+++ python/adb_auto_player/games/afk_journey/run.py
@@ -402,7 +402,6 @@ class AFKJourney(Game):
         :return:
         """
         self.start_up()
-        logging.warning("Not updated for S3 run at your own risk")
         self.store[self.STORE_MODE] = self.MODE_DURAS_TRIALS
         self.__navigate_to_duras_trials_screen()
 
@@ -418,28 +417,10 @@ class AFKJourney(Game):
             return None
 
         for banner in rate_up_banners:
-            if (
-                self.find_template_match("duras_trials/rate_up.png", grayscale=True)
-                is None
-            ):
-                self.__navigate_to_duras_trials_screen()
-                self.wait_for_template("duras_trials/rate_up.png", grayscale=True)
-
-            current_banners = self.find_all_template_matches(
-                "duras_trials/rate_up.png", grayscale=True, use_previous_screenshot=True
-            )
-
-            if current_banners is None:
-                logging.warning(
-                    "Dura's Trials Rate Up banners could not be found, Stopping"
-                )
-                return None
-
-            if len(current_banners) != len(rate_up_banners):
-                logging.warning("Dura's Trials schedule changed, Stopping")
-                return None
-
+            self.__navigate_to_duras_trials_screen()
             self.__handle_dura_screen(*banner)
+            self.__navigate_to_duras_trials_screen()
+            self.__handle_dura_screen(*banner, nightmare_mode=True)
 
         return None
 
@@ -447,49 +428,75 @@ class AFKJourney(Game):
         logging.info("Navigating to Dura's Trial select")
 
         def check_for_duras_trials_label() -> bool:
-            label = self.find_template_match("duras_trials/label.png")
-            return label is not None
+            match = self.find_template_match("duras_trials/featured_heroes.png")
+            return match is not None
 
         self.__navigate_to_default_state(check_callable=check_for_duras_trials_label)
 
-        duras_trials_label = self.find_template_match(
-            "duras_trials/label.png", use_previous_screenshot=True
+        featured_heroes = self.find_template_match(
+            "duras_trials/featured_heroes.png",
+            use_previous_screenshot=True,
         )
-        if duras_trials_label is None:
-            logging.info("Clicking Battle Modes button")
-            self.click(460, 1830, scale=True)
-            duras_trials_label = self.wait_for_template(
-                "duras_trials/label.png", timeout_message="Could not find Dura's Trials"
-            )
+        if featured_heroes is not None:
+            return
+
+        logging.info("Clicking Battle Modes button")
+        self.click(460, 1830, scale=True)
+        duras_trials_label = self.wait_for_template("duras_trials/label.png")
         self.click(*duras_trials_label)
+        featured_heroes = self.wait_for_template("duras_trials/featured_heroes.png")
+        self.click(*featured_heroes)
         return None
 
-    def __handle_dura_screen(self, x: int, y: int) -> None:
+    def __handle_dura_screen(
+        self, x: int, y: int, nightmare_mode: bool = False
+    ) -> None:
         # y+100 clicks closer to center of the button instead of rate up text
-        self.click(x, y + 100, scale=True)
+        offset = int(self.get_scale_factor() * 100)
+        self.click(x, y + offset)
 
         count: int = 0
         while True:
-            self.wait_for_any_template(
+            template, _, _ = self.wait_for_any_template(
                 ["records.png", "duras_trials/battle.png", "duras_trials/sweep.png"]
             )
-            template, x, y = self.wait_for_any_template(
-                ["records.png", "duras_trials/battle.png", "duras_trials/sweep.png"]
-            )
-            match template:
-                case "duras_trials/sweep.png":
-                    logging.info("Dura's Trial already cleared")
+
+            if nightmare_mode and template != "records.png":
+                nightmare = self.find_template_match("duras_trials/nightmare.png")
+                if nightmare is None:
+                    logging.warning("Nightmare Button not found")
                     return None
-                case "duras_trials/battle.png":
-                    self.click(x, y, scale=True)
-                case "records.png":
-                    pass
+                self.click(*nightmare)
+                template, x, y = self.wait_for_any_template(
+                    [
+                        "duras_trials/nightmare_swords.png",
+                        "duras_trials/cleared.png",
+                    ]
+                )
+                match template:
+                    case "duras_trials/nightmare_swords.png":
+                        self.click(x, y)
+                    case "duras_trials/cleared.png":
+                        logging.info("Nightmare Trial already cleared")
+                        return None
+            else:
+                template, x, y = self.wait_for_any_template(
+                    ["records.png", "duras_trials/battle.png", "duras_trials/sweep.png"]
+                )
+                match template:
+                    case "duras_trials/sweep.png":
+                        logging.info("Dura's Trial already cleared")
+                        return None
+                    case "duras_trials/battle.png":
+                        self.click(x, y)
+                    case "records.png":
+                        pass
 
             result = self.handle_battle_screen(
                 self.get_config().duras_trials.use_suggested_formations
             )
 
-            if result is True:
+            if result is True and not nightmare_mode:
                 self.wait_for_template("duras_trials/first_clear.png")
                 next_button = self.find_template_match("next.png")
                 if next_button is not None:
@@ -502,6 +509,14 @@ class AFKJourney(Game):
                 else:
                     logging.info("Dura's Trial completed")
                     return None
+
+            if result is True and nightmare_mode:
+                count += 1
+                logging.info(f"Nightmare Trials pushed: {count}")
+                if self.find_template_match("duras_trials/continue_gray.png"):
+                    logging.info("Nightmare Trial completed")
+                    return None
+                continue
             logging.info("Dura's Trial failed")
             return None
         return None
