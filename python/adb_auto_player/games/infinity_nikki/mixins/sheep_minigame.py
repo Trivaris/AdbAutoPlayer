bb275c04418742580c7286c7d143b75513184e47
yulesxoxo <189644144+yulesxoxo@users.noreply.github.com>
2025-02-28 21:37:08 +0100

    stop farming sheep minigame when bling cap has been reached

--- python/adb_auto_player/games/infinity_nikki/mixins/sheep_minigame.py
+++ python/adb_auto_player/games/infinity_nikki/mixins/sheep_minigame.py
@@ -21,17 +21,23 @@ class SheepMinigameMixin(InfinityNikkiBase, ABC):
         # 160000 / 1350 =~ 119
         bling_visible = False
         count = 0
+        history_visible_before_bling_count = 0
         while count < config.runs:
             if self.find_template_match("conversation/history.png"):
+                history_visible_before_bling_count += 1
                 bling_visible = False
                 self.click(1250, 575, scale=True)
             if self.find_template_match("rewards/bling.png", threshold=0.7):
+                history_visible_before_bling_count = 0
                 if not bling_visible:
                     count += 1
                     logging.info(f"Minigame clear #{count}")
                     bling_visible = True
                 logging.debug("Bling visible")
                 self.click(950, 950, scale=True)
+            if history_visible_before_bling_count > 20:
+                logging.warning("Exiting because Bling cap has been reached.")
+            print(history_visible_before_bling_count)
             sleep(0.5)
 
         logging.info("Done")
