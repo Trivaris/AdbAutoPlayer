793e348c6854e47d13e8aaf5efbd3b8dbaee9e6f
yulesxoxo <189644144+yulesxoxo@users.noreply.github.com>
2025-03-04 15:47:08 +0100

    fewer logs during active game polling

--- python/adb_auto_player/main.py
+++ python/adb_auto_player/main.py
@@ -100,7 +100,7 @@ def __print_gui_games_menu() -> None:
 def __print_running_game() -> None:
     running_game = __get_running_game()
     if running_game:
-        logging.critical(f"Running game: {running_game}")
+        logging.debug(f"Running game: {running_game}")
     else:
         logging.debug("No running game")
     return None
