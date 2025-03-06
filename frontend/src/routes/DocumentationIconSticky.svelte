10a8eaa388db98bdfc41bb4ede97ed97c7722b19
yulesxoxo <189644144+yulesxoxo@users.noreply.github.com>
2025-03-06 20:22:34 +0100

    refactor DocumentationIconSticky.svelte

--- frontend/src/routes/DocumentationIconSticky.svelte
+++ frontend/src/routes/DocumentationIconSticky.svelte
@@ -1,23 +1,15 @@
-<div class="documentation-icon-sticky">
+<div class="fixed top-0 left-0 z-50 m-2 cursor-pointer select-none">
   <a href="https://yulesxoxo.github.io/AdbAutoPlayer/" target="_blank">
-    <img
-      src="/icons/help-circle.svg"
-      alt="Documentation"
-      width="24"
-      height="24"
-      draggable="false"
-    />
+    <span
+      class="items-center justify-center badge-icon preset-filled-secondary-50-950"
+    >
+      <img
+        src="/icons/help-circle.svg"
+        alt="Documentation"
+        width="24"
+        height="24"
+        draggable="false"
+      />
+    </span>
   </a>
 </div>
-
-<style>
-  .documentation-icon-sticky {
-    user-select: none;
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: 1000;
-    margin: 10px;
-    cursor: pointer;
-  }
-</style>
