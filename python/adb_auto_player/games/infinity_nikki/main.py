9624eae8d6c703fb47dede3656861bec12fcc636
Yules <189644144+yulesxoxo@users.noreply.github.com>
2025-03-10 22:33:52 +0100

    Accordion Menu, Tooltip support, prettier Modal, Button: Display Size to 1080x1920
    * (error) => (e)
    
    * Accordion and Tooltips
    
    * game_gui.go
    
    * python ipc update
    
    * Modal
    
    * increase modal margin
    
    * fix test
    
    * remove updated completed alert
    
    * _Category str enum
    
    * update tooltip
    
    * add hr to last accordion item too
    
    * dosctring enum change enum name

--- python/adb_auto_player/games/infinity_nikki/main.py
+++ python/adb_auto_player/games/infinity_nikki/main.py
@@ -1,11 +1,19 @@
 """Infinity Nikki Main Module."""
 
+from enum import StrEnum
+
 from adb_auto_player.command import Command
 from adb_auto_player.games.infinity_nikki.config import Config
 from adb_auto_player.games.infinity_nikki.mixins.sheep_minigame import (
     SheepMinigameMixin,
 )
-from adb_auto_player.ipc.game_gui import GameGUIOptions
+from adb_auto_player.ipc.game_gui import GameGUIOptions, MenuOption
+
+
+class ModeCategory(StrEnum):
+    """Enumeration for mode categories used in the GUIs accordion menu."""
+
+    MINIGAMES = "Minigames"
 
 
 class InfinityNikki(
@@ -18,9 +26,14 @@ class InfinityNikki(
         return [
             Command(
                 name="SheepMinigame",
-                gui_label="Sheep Minigame",
                 action=self.afk_sheep_minigame,
                 kwargs={},
+                menu_option=MenuOption(
+                    label="Sheep Minigame",
+                    category=ModeCategory.MINIGAMES,
+                    tooltip='This runs the Sheep Minigame till "Runs" count or '
+                    "Bling cap is reached.",
+                ),
             ),
         ]
 
@@ -30,5 +43,6 @@ class InfinityNikki(
             game_title="Infinity Nikki",
             config_path="infinity_nikki/InfinityNikki.toml",
             menu_options=self._get_menu_options_from_cli_menu(),
+            categories=[ModeCategory.MINIGAMES],
             constraints=Config.get_constraints(),
         )
