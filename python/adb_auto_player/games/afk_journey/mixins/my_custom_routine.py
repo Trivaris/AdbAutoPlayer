82f0a7a04f719b6e3f20e20ad87dcae2debd4a51
yulesxoxo <189644144+yulesxoxo@users.noreply.github.com>
2025-05-29 23:03:05 +0200

    afkj move custom routine next to stop action

--- python/adb_auto_player/games/afk_journey/mixins/my_custom_routine.py
+++ python/adb_auto_player/games/afk_journey/mixins/my_custom_routine.py
@@ -1,15 +1,12 @@
 from adb_auto_player.decorators.register_command import GuiMetadata, register_command
 from adb_auto_player.games.afk_journey.base import AFKJourneyBase
-from adb_auto_player.games.afk_journey.gui_category import AFKJCategory
 
 
 class AFKJCustomRoutine(AFKJourneyBase):
     """Wrapper to register custom routines for AFKJourney."""
 
     @register_command(
-        gui=GuiMetadata(
-            label="My Custom Routine", category=AFKJCategory.WIP_PLEASE_TEST
-        ),
+        gui=GuiMetadata(label="My Custom Routine"),
         name="AFKJCustomRoutine",
     )
     def _execute(self):
