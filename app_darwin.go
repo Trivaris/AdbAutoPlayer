8b30368658a1abc4be982e35dc61e8256245e696
yulesxoxo <189644144+yulesxoxo@users.noreply.github.com>
2025-06-26 14:01:32 +0200

    do not log hotkey already registered

--- app_darwin.go
+++ app_darwin.go
@@ -6,13 +6,16 @@ import (
 	"adb-auto-player/internal"
 	"github.com/wailsapp/wails/v2/pkg/runtime"
 	"golang.design/x/hotkey"
+	"strings"
 )
 
 func registerGlobalHotkeys(a *App) {
 	// Register CTRL+CMD+C
 	hk := hotkey.New([]hotkey.Modifier{hotkey.ModCtrl, hotkey.ModCmd, hotkey.ModShift}, hotkey.KeyC)
 	if err := hk.Register(); err != nil {
-		runtime.EventsEmit(a.ctx, "failed-to-register-global-stop-hotkey", err.Error())
+		if !strings.Contains(err.Error(), "already registered") {
+			runtime.EventsEmit(a.ctx, "failed-to-register-global-stop-hotkey", err.Error())
+		}
 		return
 	}
 
