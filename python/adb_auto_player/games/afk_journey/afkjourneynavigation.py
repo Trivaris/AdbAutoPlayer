7098e46c8d0140077f6cc4a2659c28286085ceaa
yulesxoxo <189644144+yulesxoxo@users.noreply.github.com>
2025-06-29 11:52:07 +0200

    update crop for time_of_day

--- python/adb_auto_player/games/afk_journey/afkjourneynavigation.py
+++ python/adb_auto_player/games/afk_journey/afkjourneynavigation.py
@@ -156,7 +156,7 @@ class AFKJourneyNavigation(Game, ABC):
         return (
             self.game_find_template_match(
                 "navigation/time_of_day.png",
-                crop_regions=CropRegions(left=0.6, right=0.1, bottom=0.8),
+                crop_regions=CropRegions(left=0.6, bottom=0.7),
             )
             is not None
         )
