83894e878016ba55756dd2420441e4252d180bc8
yulesxoxo <189644144+yulesxoxo@users.noreply.github.com>
2025-08-07 14:32:41 +0200

    AFKJ: Handle scenario where afk reward collection gets blocked by popup

--- python/adb_auto_player/games/afk_journey/custom_routine/claim_afk_rewards.py
+++ python/adb_auto_player/games/afk_journey/custom_routine/claim_afk_rewards.py
@@ -18,10 +18,12 @@ class ClaimAFKRewards(AFKJourneyBase):
 
         logging.info("Tapping AFK Rewards chest.")
         for _ in range(3):
-            self.tap(Point(x=550, y=1080), scale=True, log_message=None)
             self.tap(Point(x=520, y=1400), scale=True, log_message=None)
+            self.tap(Point(x=550, y=1080), scale=True, log_message=None)
             sleep(1)
         sleep(1)
+        # Make sure the Popup doesn't block collecting AFK Rewards
+        self.tap(Point(x=550, y=1080), scale=True, log_message=None)
         if self.get_config().claim_afk_rewards:
             for _ in range(3):
                 self.tap(Point(x=770, y=500), scale=True, log_message=None)
